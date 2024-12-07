1f1dc339d9969f913a5d7b3c8a5ea5800cf122c7
Anushka <goyalanushka28@gmail.com>
2024-12-07 13:23:39 +0530

    changes in routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const routes = (
   <Router>
     <Routes>
     <Route path="/" element={<Navigate to="/login" />} />
-      <Route path='/dashboard' exact element={<Home/>}/>
-      <Route path='/login' exact element={<Login/>}/>
-      <Route path='/signup' exact element={<SignUp/>}/>
+      <Route path='/dashboard'  element={<Home/>}/>
+      <Route path='/login'  element={<Login/>}/>
+      <Route path='/signup'  element={<SignUp/>}/>
     </Routes>
   </Router>
 )
