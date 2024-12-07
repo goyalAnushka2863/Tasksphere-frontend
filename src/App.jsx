c337da29b904cae218c4bf0dcf0b7995d87f7d75
Anushka <goyalanushka28@gmail.com>
2024-12-07 12:47:25 +0530

    Changed route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home/home'
 import Login from './pages/login/login'
 import SignUp from './pages/signup/signup'
 const routes = (
   <Router>
     <Routes>
+    <Route path="/" element={<Navigate to="/login" />} />
       <Route path='/dashboard' exact element={<Home/>}/>
       <Route path='/login' exact element={<Login/>}/>
       <Route path='/signup' exact element={<SignUp/>}/>
